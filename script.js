3aa24d77c71eb1b845d7eb8ac0b15376aa09a1bb
Jacobtkristensen <115540527+Jacobtkristensen@users.noreply.github.com>
2024-01-29 15:01:16 +0100

    fully functional guess number game, with reset button and clearing of previous guesses.

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
 "use strict";
 
+let guess = "";
+let generatedNumber = "";
+let numberOfGuesses = 0;
 window.addEventListener("load", start);
 
 function start() {
     console.log("Javascript running.")
+generatedNumber = generateRandomNumber();
+document.querySelector("form").addEventListener("submit", receiveUserInput);
+document.querySelector("#resetButton").addEventListener("click", resetGame);
 }
 function generateRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
 
 }
-function receiveUserInput() {
+function receiveUserInput(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const value = form.guess.valueAsNumber;
+    console.log(value);
+    checkGuess(value);
+    form.guess.value = "";
+}
+function checkGuess(guess) {
+    if(guess === generatedNumber) {
+        guessIsCorrect(guess);
+    } else if (guess > generatedNumber) {
+        guessIsTooHigh(guess);
+    } else {
+        guessIsTooLow(guess);
+    }
+}
+function guessIsCorrect(guess) {
+    console.log("Correct");
+    const list = document.querySelector(".guesslist");
+    const html = `<li>You guessed ${guess} - Correct! Congratulations.</li>`;
+    list.insertAdjacentHTML("beforeend", html);
+    const reset = document.querySelector("#resetButton");
+    reset.style.display = "flex";
+    document.querySelector("form").style.display = "none";    
 
 }
-function checkGuess() {
-
+function guessIsTooHigh(guess) {
+    console.log("Too high");
+    const list = document.querySelector(".guesslist");
+    const html = `<li>You guessed ${guess} - that was too high! Try again</li>`;
+    list.insertAdjacentHTML("beforeend", html);
 }
-function guessIsCorrect() {
-
+function guessIsTooLow(guess) {
+    const list = document.querySelector(".guesslist");
+    const html = `<li>You guessed ${guess} - that was too low! Try again</li>`;
+    list.insertAdjacentHTML("beforeend", html);
 }
-function guessIsTooHigh() {
-
-}
-function guessIsTooLow() {
-
+function resetGame() {
+    document.querySelector("form").style.display = "block";    
+    document.querySelector(".guesslist").innerHTML = "";
+    document.querySelector("#resetButton").style.display = "none";
 }
